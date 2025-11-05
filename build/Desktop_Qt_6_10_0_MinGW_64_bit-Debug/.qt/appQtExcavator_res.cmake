877606dc60c6333dfe4eb29c891a5d314b7688a7
KaixuanLee <92129106+KaixuanLee@users.noreply.github.com>
2025-11-05 09:39:46 -0500

    Delete build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/appQtExcavator_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appQtExcavator")
-set(working_dir "D:/About-Project/QtExcavator")
-set(src_and_dest_list
-
-)
-set(timestamp_file "D:/About-Project/QtExcavator/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/appQtExcavator_res.txt")
